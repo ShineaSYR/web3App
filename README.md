8d4a78713102913af230bc7e4f4cad2ae1860611
p_yrongshan <p_yrongshan@tencent.com>
2024-09-11 16:42:04 +0800

    feat(): 项目初始化 & web3 i

---
+++ README.md
@@ -0,0 +1,18 @@
+# Web3应用
+
+基于vite5+vue3创建项目(node 18+)
+```shell
+pnpm create vite web3App --template vue
+
+cd web3App
+pnpm install
+pnpm run dev
+```
+
+引入web3js
+```
+pnpm install web3
+```
+
+
+
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+// import Web3 from 'web3'
+import { Web3 } from 'web3';
+
+const web3 = new Web3();
+// const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+console.log('web3', web3)
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
